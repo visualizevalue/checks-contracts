5789c148adfeaa6a8eb8f93564cf9b42c133dc52
jalil <jalil@1001.digital>
2023-01-28 03:51:37 +0100

    Log library addresses

--- helpers/deploy.ts
+++ helpers/deploy.ts
@@ -2,10 +2,12 @@ export const deployChecksWithLibraries = async (ethers) => {
   const Utils = await ethers.getContractFactory('Utils')
   const utils = await Utils.deploy()
   await utils.deployed()
+  console.log(`     Deployed Utils at ${utils.address}`)
 
   const EightyColors = await ethers.getContractFactory('EightyColors')
   const eightyColors = await EightyColors.deploy()
   await eightyColors.deployed()
+  console.log(`     Deployed EightyColors at ${eightyColors.address}`)
 
   const ChecksArt = await ethers.getContractFactory('ChecksArt', {
     libraries: {
@@ -15,6 +17,7 @@ export const deployChecksWithLibraries = async (ethers) => {
   })
   const checksArt = await ChecksArt.deploy()
   await checksArt.deployed()
+  console.log(`     Deployed ChecksArt at ${checksArt.address}`)
 
   const ChecksMetadata = await ethers.getContractFactory('ChecksMetadata', {
     libraries: {
@@ -24,6 +27,7 @@ export const deployChecksWithLibraries = async (ethers) => {
   })
   const checksMetadata = await ChecksMetadata.deploy()
   await checksMetadata.deployed()
+  console.log(`     Deployed ChecksMetadata at ${checksMetadata.address}`)
 
   const ChecksOriginals = await ethers.getContractFactory('Checks', {
     libraries: {
@@ -34,6 +38,7 @@ export const deployChecksWithLibraries = async (ethers) => {
   })
   const checks = await ChecksOriginals.deploy()
   await checks.deployed()
+  console.log(`     Deployed ChecksOriginals at ${checks.address}`)
 
   return {
     checks
--- tasks/deploy.ts
+++ tasks/deploy.ts
@@ -1,7 +1,7 @@
 import { task } from 'hardhat/config'
 import { deployChecksWithLibraries } from '../helpers/deploy'
 
-task('deploy-testing', 'Deploys all contracts for testing', async (_, hre) => {
+task('deploy', 'Deploys all contracts for testing', async (_, hre) => {
   const { checks } = await deployChecksWithLibraries(hre.ethers)
 
   console.log(`Successfully deployed Checks at ${checks.address}`)
