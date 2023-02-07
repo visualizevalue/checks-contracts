620e0b645cf67057eac235afb8bd1abf18b008b7
jalil <jalil@1001.digital>
2023-02-07 01:06:51 +0100

    Reset WithEpochs.sol

--- contracts/standards/WithEpochs.sol
+++ contracts/standards/WithEpochs.sol
@@ -35,39 +35,30 @@ abstract contract WithEpochs {
 
     mapping(uint256 => Epoch) public epochs;
 
-    function initializeOrResolveEpoch() public {
+    function resolveEpochIfNeeded() public {
         Epoch storage currentEpoch = epochs[epochIndex];
 
         if (
-            // If epoch has not been commited,
+            //If epoch has not been commited
             currentEpoch.commited == false ||
-            // Or the reveal commitment timed out.
+            //If epoch has not been revealed, but the block is too far away (256 block)
             (currentEpoch.revealed == false && currentEpoch.revealBlock < block.number - 256)
         ) {
-            // This means the epoch has not been commited, OR the epoch was commited but has expired.
-            initializeEpoch();
+            //This means the epoch has not been commited, OR the epoch was commited but has expired.
+
+            //Set commited to true, and record the reveal block
+            currentEpoch.revealBlock = uint64(block.number + 5);
+            currentEpoch.commited = true;
+
         } else if (block.number > currentEpoch.revealBlock) {
-            // Epoch has been commited and is within range to be revealed.
-            resolveEpoch();
-            initializeEpoch();
+            //Epoch has been commited and is within range to be revealed.
+            //Set its randomness to the target block
+            currentEpoch.randomness = uint128(uint256(blockhash(currentEpoch.revealBlock)) % (2 ** 128 - 1));
+            currentEpoch.revealed = true;
+
+            epochIndex++;
+
+            return resolveEpochIfNeeded();
         }
-    }
-
-    function initializeEpoch() internal {
-        Epoch storage currentEpoch = epochs[epochIndex];
-
-        // Set commited to true, and record the reveal block.
-        currentEpoch.revealBlock = uint64(block.number + 5);
-        currentEpoch.commited = true;
-    }
-
-    function resolveEpoch() internal {
-        Epoch storage currentEpoch = epochs[epochIndex];
-
-        // Set its randomness to the target block
-        currentEpoch.randomness = uint128(uint256(blockhash(currentEpoch.revealBlock)) % (2 ** 128 - 1));
-        currentEpoch.revealed = true;
-
-        epochIndex++;
     }
 }
