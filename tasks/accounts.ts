9118a51b9306d5126f679c9a748ff70d53e44e12
jalil <jalil@1001.digital>
2023-02-09 12:18:10 +0100

    Add task to fund account

--- tasks/accounts.ts
+++ tasks/accounts.ts
@@ -1,4 +1,7 @@
+import { parseEther } from "ethers/lib/utils";
 import { task } from "hardhat/config";
+import { JALIL, VV } from "../helpers/constants";
+import { impersonate } from "../helpers/impersonate";
 
 task('accounts', 'Prints the list of accounts', async (_, hre) => {
   const accounts = await hre.ethers.getSigners()
@@ -6,4 +9,12 @@ task('accounts', 'Prints the list of accounts', async (_, hre) => {
   for (const account of accounts) {
     console.log(account.address)
   }
+})
+
+task('fund-jalil', 'Funds jalil for testing', async (_, hre) => {
+  const vv = await impersonate(VV, hre)
+  const jalil = await impersonate(JALIL, hre)
+
+  // Fund me :kek:
+  await vv.sendTransaction({ to: JALIL, value: parseEther('1') })
 })
