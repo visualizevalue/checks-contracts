c3108690776e0e56de926f9a3c4c087a4d89233a
jalil <jalil@1001.digital>
2023-02-11 15:42:33 +0100

    Update description

--- contracts/libraries/ChecksMetadata.sol
+++ contracts/libraries/ChecksMetadata.sol
@@ -36,7 +36,7 @@ library ChecksMetadata {
         bytes memory metadata = abi.encodePacked(
             '{',
                 '"name": "Checks ', Utilities.uint2str(tokenId), '",',
-                '"description": "This artwork is notable.",',
+                '"description": "This artwork may or may not be notable.",',
                 '"image": ',
                     '"data:image/svg+xml;base64,',
                     Base64.encode(svg),
