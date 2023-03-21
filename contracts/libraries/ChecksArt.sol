8012e24743de7990fe5ae72075775a15cfc6a4c9
jalil <jalil@1001.digital>
2023-03-21 18:22:02 +0100

    Sort imports

--- contracts/libraries/ChecksArt.sol
+++ contracts/libraries/ChecksArt.sol
@@ -1,8 +1,8 @@
 //SPDX-License-Identifier: MIT
 pragma solidity ^0.8.17;
 
-import "./EightyColors.sol";
 import "../interfaces/IChecks.sol";
+import "./EightyColors.sol";
 import "./Utilities.sol";
 
 /**
